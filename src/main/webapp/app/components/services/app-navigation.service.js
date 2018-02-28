fcac4eeeee3319c06b9c2d75948b7e10a5221488
kuterginav <aleksei.kutergin@saprun.com>
2018-02-28 13:31:35 +0500

    Решил проблему с уведомлением подписчика о начальном выделении

--- src/main/webapp/app/components/services/app-navigation.service.js
+++ src/main/webapp/app/components/services/app-navigation.service.js
@@ -5,21 +5,24 @@
         .module('coreModule')
         .factory('appNavigationService', function (rx) {
             var service = {};
+
             service._activeModuleObserver = new rx.Subject();
+            service._cachedActiveModule = null;
 
             service.changeActiveModule = function (module) {
                 if(service._activeModuleObserver.observers.length) {
                     service._activeModuleObserver.onNext(module);
                 } else {
-                    // TODO: 1. Закешить модуль
-                    // TODO: 2. Дождать первого подписчика
-                    // TODO: 3. Вызывать onNext(...)
-                    // TODO: 4. Очитстить кэш
+                    service._cachedActiveModule = module;
                 }
             };
 
             service.subscribeActiveModule = function (onNext) {
                 service._activeModuleObserver.subscribe(onNext);
+                if(service._cachedActiveModule) {
+                    service._activeModuleObserver.onNext(service._cachedActiveModule);
+                    service._cachedActiveModule = null;
+                }
             };
 
             return service;
